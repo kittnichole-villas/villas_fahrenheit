1d71e9e23301b6714a5f063c5a84d27821099297
kittnichole-villas <kittnicholevillas@gmail.com>
2026-09-17 17:02:03 +0800

    added the code

--- fahrenheit.c
+++ fahrenheit.c
@@ -1,6 +1,20 @@
 int main(){
 	
-	printf("Temperature");
+	//declare variables 
+	float fahrenheit;
+	float celsius;
+
+	//output: ask for the temperature in Fahrenheit 
+	printf("Enter temperature in Fahrenheit: ");
+
+	//input: temperature in Fahrenheit 
+	scanf("%.2f", &fahrenheit);
+
+	//process: convert Fahrenheit into Celsius 
+	celsius = (fahrenheit - 32) * 5 / 9;
+
+	//output: temperature in Celsius 
+	printf("Temperature in Celsius: %.2f", celsius);
 	
 	return 0;
 	
